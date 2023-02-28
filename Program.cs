93bbd74ebccf02b48ca68ebd49fab9c262a9bf27
Jacob <46667568+jacobvd0@users.noreply.github.com>
2023-02-28 20:27:33 +1030

    comments

--- Program.cs
+++ Program.cs
@@ -13,6 +13,13 @@ namespace ClassesExercise // Note: actual namespace depends on the project name.
             Weapon gun = new Weapon("Gun", 4338724); // Makes a new gun with 4338724 ammo slots and names it Gun (dont increase or ram go bye bye)
             gun.ammolist[4].ModifyBySpeedAndDirection(); // Modifies the projectile location using the direction and speed
 
+            // Another gun to test how gun firing works
+            Weapon testGun = new Weapon("Gun", 2);
+            testGun.useWeapon();
+            testGun.useWeapon();
+            testGun.useWeapon();
+
+
             Console.ReadKey(true);
         }
     }
--- Weapon.cs
+++ Weapon.cs
@@ -25,5 +25,28 @@ namespace ClassesExercise
                 i++;
             }
         }
+
+        public void useWeapon() // Uses up one ammo slot and sets it to null (also checks if there is enough remaning (non null) ammo left before doing so)
+        {
+            // variables needed for foreach loop
+            bool isAmmo = false;
+            int i = 0;
+
+            foreach (var projectile in ammolist) // Loops through all the slots in the ammo list
+            {
+                if (ammolist[i] != null) // Checks if there is ammo in that slot
+                {
+                    ammolist[i] = null; // Removes ammo from the slot
+                    isAmmo = true; // Sets a variable which says if it has detected ammo or not
+                    Console.WriteLine($"Your {name} has been fired"); // Tell the user that the gun has been fired
+                    break; // End the foreach loop early as it has already found the ammo it needed
+                }
+                i++;
+            }
+            if (!isAmmo) // Checks if the isAmmo is not true (should only be set to this if no ammo is found in the table)
+            {
+                Console.WriteLine($"Your {name} has no ammo left");
+            }
+        }
     }
 }
