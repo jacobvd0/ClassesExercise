9d1f28ed03785ad608e2f054f7ede6a75a285656
Jacob <46667568+jacobvd0@users.noreply.github.com>
2023-02-28 19:50:59 +1030

    a

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace ClassesExercise // Note: actual namespace depends on the project name.
             Projectile projectile = new Projectile(2.0f, new Vector2(0.0f, 0.0f), new Vector2(5.0f, 2.0f)); // Makes a new projectile
             projectile.ModifyBySpeedAndDirection(); // Modifies the projectile location using the direction and speed
 
-            Weapon gun = new Weapon("Gun", 4338724); // Makes a new gun with 4338724 ammo slots and names it Gun
+            Weapon gun = new Weapon("Gun", 994338724); // Makes a new gun with 4338724 ammo slots and names it Gun
             gun.ammolist[4].ModifyBySpeedAndDirection(); // Modifies the projectile location using the direction and speed
 
             Console.ReadKey(true);
